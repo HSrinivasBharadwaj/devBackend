fa26e98e869480919362e829f1e0015e58032f51
HSrinivasBharadwaj <srinivasbharadwaj.hullur@gmail.com>
2025-06-10 21:14:31 +0530

    Created the requests received and user connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,18 +7,39 @@ const userRouter = express.Router();
 userRouter.get("/user/requests/received", validateAuth, async (req, res) => {
   try {
     const loggedinuser = req.user;
-    const getPendingConnectionRequests = await connectionRequest.find({
-      toUserId: loggedinuser._id,
-      status: "interested",
-    }).populate("fromUserId",["firstName","lastName","photoUrl"]);
+    const getPendingConnectionRequests = await connectionRequest
+      .find({
+        toUserId: loggedinuser._id,
+        status: "interested",
+      })
+      .populate("fromUserId", ["firstName", "lastName", "photoUrl"]);
+    return res.status(200).json({
+      message: "Requests fetched successfully",
+      data: getPendingConnectionRequests,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//Get all the connections received - like who is connected to me
+//validateAuth
+//find connection request toUserId: loggedInUser._id and fromtoUserId,status: "accepted"
+userRouter.get("/user/connections", validateAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const getConnection = await connectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", ["firstName","lastName", "photoUrl"]);
+    const data = getConnection.map(data => data.fromUserId)
     return res
       .status(200)
-      .json({
-        message: "Requests fetched successfully",
-        data: getPendingConnectionRequests,
-      });
+      .json({ message: "Fetched all connections", data: data });
   } catch (error) {
-    console.log(error)
     return res.status(500).json({ message: "Internal server error" });
   }
 });
